a3c124821f7fa14f033c81f57e170b9a9ec076cc
Brandon Le <greenunknown0@gmail.com>
2021-02-06 11:48:07 -0800

    Resizing thumbnails works. Fixing refresh/redraw issues by aliging elements to the top. Making app resizable and max size.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Author: Brandon Le
+# Purpose: Simple desktop app to create a gesture gallery from a folder of images
 # Modified from https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Img_Viewer.py
 # Modified from https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Desktop_Widget_Timer.py
 
@@ -16,8 +18,40 @@ def time_as_int():
 def get_img_data(f, maxsize=(1200, 850), first=False):
     """Generate image data using PIL
     """
+
+    # TODO save thumbnails in temporary directory and delete this directory when program exits
+    #   to avoid redoing image processing work
+
     img = Image.open(f)
-    img.thumbnail(maxsize)
+    img_size = img.size
+    img_width, img_height = img_size
+    # if the original width is larger than the max width
+    if img_width > maxsize[0]:
+        scale_factor_width = maxsize[0] / img_width
+
+        # if the original height is larger than the max height after scaling by the width,
+        #   scale the image to this scale factor instead
+        if (img_height * scale_factor_width) > maxsize[1]:
+            scale_factor_height = maxsize[1] / img_height
+            img.thumbnail((img_width * scale_factor_height, img_height * scale_factor_height))
+        # else if the original height is less than the max height after scaling,
+        #   scale by just the width
+        else:
+            img.thumbnail((img_width * scale_factor_width, img_height * scale_factor_width))
+    # else if the original width is smaller
+    else:
+        # if the original height is larger, scale by just the height
+        if img_height > maxsize[1]:
+            scale_factor_height = maxsize[1] / img_height
+            img.thumbnail((img_width * scale_factor_height, img_height * scale_factor_height))
+        # else the original width and height are smaller so enlarge them by the smallest scale factor
+        else:
+            scale_factor_width = maxsize[0] / img_width
+            scale_factor_height = maxsize[1] / img_height
+            scale_factor = min(scale_factor_width, scale_factor_height)
+            img.thumbnail((img_width * scale_factor, img_height* scale_factor))
+
+    # img.thumbnail(maxsize)
     if first:                     # tkinter is inactive the first time
         bio = io.BytesIO()
         img.save(bio, format="PNG")
@@ -30,7 +64,7 @@ if __name__ == "__main__":
     current_time, paused_time, paused = 0, 0, False
     start_time = time_as_int()
 
-    # Get the folder containin:g the images from the user
+    # Get the folder containing the images from the user
     folder = sg.popup_get_folder('Image folder to open', default_path='')
     if not folder:
         sg.popup_cancel('Cancelling')
@@ -64,10 +98,12 @@ if __name__ == "__main__":
     file_num_display_elem = sg.Text('File 1 of {}'.format(num_files), size=(15, 1))
 
     # define layout, show and read the form
-    col = [[filename_display_elem],
-           [image_elem]]
+    # col = [[filename_display_elem],
+    #        [image_elem]]
+    col = [[image_elem]]
 
-    col_files = [[sg.Listbox(values=fnames, change_submits=True, size=(60, 30), key='listbox')],
+    col_files = [[filename_display_elem],
+                [sg.Listbox(values=fnames, change_submits=True, size=(60, 30), key='listbox')],
                  [sg.Button('Prev', size=(8, 2)), sg.Button('Next', size=(8, 2)), file_num_display_elem],
                  [sg.Text('')],
                  [sg.Text('', size=(8, 2), font=('Helvetica', 20),
@@ -77,22 +113,23 @@ if __name__ == "__main__":
                   sg.Exit(button_color=('white', 'firebrick4'), key='Exit')]
                  ]
 
-    layout = [[sg.Column(col_files), sg.Column(col)]]
+    layout = [[sg.Column(vertical_alignment='top', layout=col_files), sg.Column(vertical_alignment='top', layout=col)]]
 
     window = sg.Window('Image Browser', layout, return_keyboard_events=True,
-                       location=(0, 0), use_default_focus=False)
+                       location=(0, 0), size=(1920, 1080), resizable=True, use_default_focus=False)
 
     # loop reading the user input and displaying image, filename
     NEXTIMGTIMEOUT = 1000
     i = 0
     while True:
-        print(paused)
+        print(sg.Window.get_screen_size())
+        # print(paused)
         if not paused:
             event, values = window.read(timeout=10)
             current_time = time_as_int() - start_time
         else:
             event, values = window.read()
-        print(f"Event: {event}, Values: {values}")
+        # print(f"Event: {event}, Values: {values}")
         # perform button and keyboard operations
 
         if event in (sg.WIN_CLOSED, 'Exit'):
@@ -127,7 +164,7 @@ if __name__ == "__main__":
         else:
             filename = os.path.join(folder, fnames[i])
 
-        print(f"current time = {current_time}")
+        # print(f"current time = {current_time}")
         if current_time > NEXTIMGTIMEOUT:
             print("\n\n\nRESET\n\n\n")
             paused_time = start_time = time_as_int()
@@ -138,9 +175,11 @@ if __name__ == "__main__":
             filename = os.path.join(folder, fnames[i])
 
         # --------- Display timer in window --------
-        window['text'].update('{:02d}:{:02d}.{:02d}'.format((current_time // 100) // 60,
-                                                            (current_time // 100) % 60,
-                                                            current_time % 100))
+        # window['text'].update('{:02d}:{:02d}.{:02d}'.format((current_time // 100) // 60,
+        #                                                     (current_time // 100) % 60,
+        #                                                     current_time % 100))
+        window['text'].update('{:02d}:{:02d}'.format((current_time // 100) // 60,
+                                                     (current_time // 100) % 60))
 
         # update window with new image
         image_elem.update(data=get_img_data(filename, first=True))
@@ -148,5 +187,6 @@ if __name__ == "__main__":
         filename_display_elem.update(filename)
         # update page display
         file_num_display_elem.update('File {} of {}'.format(i+1, num_files))
+        window.Refresh()
 
     window.close()
